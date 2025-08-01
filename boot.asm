358041503da5f7220593d1f5287969b843d5115c
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-08-01 18:08:21 +0200

    first commit

---
+++ boot.asm
@@ -0,0 +1,24 @@
+; boot.asm - Bootloader simples
+BITS 16
+ORG 0x7C00
+
+start:
+    mov si, msg
+    call print_string
+    jmp $
+
+print_string:
+    mov ah, 0x0E
+.next_char:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .next_char
+.done:
+    ret
+
+msg db "Bootando ErnixOS...", 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
